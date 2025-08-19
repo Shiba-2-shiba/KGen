7c20333ef8788402bbdc0f8b08b2be2ae179bab3
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-08-19 12:38:08 +0900

    Update tipo.py

--- src/kgen/executor/tipo.py
+++ src/kgen/executor/tipo.py
@@ -369,7 +369,7 @@ def generate_with_retry(
             "min_p": 0.1,
             "top_p": 0.95,
             "top_k": 60,
-            "max_new_tokens": 1024,
+            "max_new_tokens": 512,
             "seed": seed + iter_count,
         }
         generation_setting.update(kwargs)
